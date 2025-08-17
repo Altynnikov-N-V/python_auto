cbc9b27dbc7bb76a34e6dfd5fc512318c7e4b0c2
altynnikov <altynnikov@mts.ru>
2025-08-17 14:22:46 +0300

    high-level-step-objects - поправил путь до фала

--- lesson_9_Page_object/lesson/demoga_tests/test_main.py
+++ lesson_9_Page_object/lesson/demoga_tests/test_main.py
@@ -9,18 +9,18 @@ from users import User
 def test_form_submission():
     registration_page = RegistrationPage()
     ivan = User(first_name='Olga',
-                 last_name='YA',
-                 email='name@example.com',
-                 gender="Female",
-                 phone_number='1234567891',
-                 birthday=('August', '1995', '5'),
-                 first_subject='Maths',
-                 second_subject=('p', 'Computer Science'),
-                 hobby="Reading",
-                 file_name='textfile.txt',
-                 address='Moscowskaya Street 18',
-                 user_location=('NCR', 'Delhi')
-                 )
+                last_name='YA',
+                email='name@example.com',
+                gender="Female",
+                phone_number='1234567891',
+                birthday=('August', '1995', '5'),
+                first_subject='Maths',
+                second_subject=('p', 'Computer Science'),
+                hobby="Reading",
+                file_name='textfile.txt',
+                address='Moscowskaya Street 18',
+                user_location=('NCR', 'Delhi')
+                )
 
     registration_page.open()
     registration_page.register(ivan)
