fa4a3251d98685b4c515f874c7fde03f8384855b
altynnikov <altynnikov@mts.ru>
2025-08-27 00:01:24 +0300

    add attach5

--- lesson_9_Page_object/lesson/conftest.py
+++ lesson_9_Page_object/lesson/conftest.py
@@ -6,6 +6,22 @@ from selene import Browser, Config
 
 from lesson_9_Page_object.utils import attach
 
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+chrome_options = Options()
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--remote-debugging-port=9222")
+chrome_options.add_argument("--incognito")
+chrome_options.add_argument("--no-first-run")
+chrome_options.add_argument("--no-default-browser-check")
+
+browser.config.driver_options = chrome_options
+browser.config.driver_manager = ChromeDriverManager()
+
 @pytest.fixture(scope='function')
 def setup_browser(request):
     options = Options()
