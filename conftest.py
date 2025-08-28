ee4c53a973ea2504ce3eceb6de15e5a6419be61e
altynnikov <altynnikov@mts.ru>
2025-08-28 23:55:42 +0300

    Правки 10

--- conftest.py
+++ conftest.py
@@ -1,52 +1,39 @@
 import pytest
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from selene import browser
-import tempfile
-import shutil
-import os
-from lesson_jenkins.utils import attach
 
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser():
-    # Настройка Chrome options для локального запуска
-    chrome_options = Options()
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--incognito')
-    chrome_options.add_argument('--no-first-run')
-    chrome_options.add_argument('--no-default-browser-check')
+    login = os.getenv('SELENOID_LOGIN')
+    password = os.getenv('SELENOID_PASS')
+    host = os.getenv('SELENOID_URL')
 
-    # Уникальная user-data директория чтобы избежать конфликтов
-    temp_user_data_dir = tempfile.mkdtemp()
-    chrome_options.add_argument(f'--user-data-dir={temp_user_data_dir}')
+    options = Options()
+    options.set_capability('browserName', 'chrome')
+    options.set_capability('browserVersion', '128.0')
+    options.set_capability('selenoid:options', {
+        'enableVNC': True,
+        'enableVideo': True  # Включить запись видео
+    })
 
-    # Используем установленный ChromeDriver
-    chromedriver_path = os.getenv('CHROMEDRIVER_PATH', '/usr/local/bin/chromedriver')
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@{host}/wd/hub",
+        options=options
+    )
 
-    # Установка и настройка драйвера
-    service = Service(executable_path=chromedriver_path)
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-
-    # Конфигурация Selene
     browser.config.driver = driver
     browser.config.timeout = 10
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
 
     yield
 
-    # ТЕЛО ФИКСТУРЫ ДОЛЖНО БЫТЬ ЗДЕСЬ, а не второй yield
-    # Добавление вложений Allure
+    # Добавление вложений
+    from lesson_jenkins.utils import attach
     attach.add_screenshot(browser)
     attach.add_logs(browser)
     attach.add_html(browser)
-    attach.add_video(browser)
+    attach.add_video(browser)  # Теперь видео будет работать
 
-    # Завершение
     browser.quit()
-    shutil.rmtree(temp_user_data_dir, ignore_errors=True)
