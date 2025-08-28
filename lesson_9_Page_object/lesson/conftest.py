237bf07eec46643b616f69e523d34d61b34fdafe
altynnikov <altynnikov@mts.ru>
2025-08-28 22:01:49 +0300

    Правки 1

--- lesson_9_Page_object/lesson/conftest.py
+++ lesson_9_Page_object/lesson/conftest.py
@@ -1,15 +1,14 @@
 import pytest
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
-from lesson_9_Page_object.utils import attach
 
+from ..utils import attach
 
 @pytest.fixture(scope='function')
 def setup_browser(request):
     options = Options()
-
-    # Настройки для Selenoid
     selenoid_capabilities = {
         "browserName": "chrome",
         "selenoid:options": {
@@ -17,30 +16,18 @@ def setup_browser(request):
             "enableVideo": True
         }
     }
-
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-first-run")
-    options.add_argument("--no-default-browser-check")
-
     options.capabilities.update(selenoid_capabilities)
-
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
     )
 
-    browser_instance = Browser(Config(driver))
-    browser_instance.config.window_width = 1920
-    browser_instance.config.window_height = 1080
+    browser = Browser(Config(driver))
+    yield browser
 
-    yield browser_instance
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
-    attach.add_screenshot(browser_instance)
-    attach.add_logs(browser_instance)
-    attach.add_html(browser_instance)
-    attach.add_video(browser_instance)
-
-    browser_instance.quit()
+    browser.quit()
