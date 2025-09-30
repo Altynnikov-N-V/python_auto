7901c06b2c7a69724718cb0653ac9a3d0ca4ed50
altynnikov <altynnikov@mts.ru>
2025-09-30 10:28:38 +0300

    lesson_19_ui - добавил видео

--- lesson_19_ui_1/tests/android_app/conftest.py
+++ lesson_19_ui_1/tests/android_app/conftest.py
@@ -1,40 +1,73 @@
 import pytest
+import requests
+import allure
+from appium import webdriver
 from appium.options.android import UiAutomator2Options
-from selene import browser
-import os
 
-from selenium import webdriver
+BROWSERSTACK_USERNAME = "bsuser_oH0Eba"
+BROWSERSTACK_ACCESS_KEY = "gvxzoGEYMxuigMaqnWxY"
 
+def get_browserstack_video_url(session_id):
+    url = f"https://api.browserstack.com/app-automate/sessions/{session_id}.json"
+    response = requests.get(url, auth=(BROWSERSTACK_USERNAME, BROWSERSTACK_ACCESS_KEY))
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("automation_session", {}).get("video_url")
+    return None
 
-@pytest.fixture(scope='function', autouse=True)
-def mobile_management():
-    options = UiAutomator2Options().load_capabilities({
-        # Specify device and os_version for testing
-        # "platformName": "android",
-        "platformVersion": "9.0",
-        "deviceName": "Google Pixel 3",
-
-        # Set URL of the application under test
+@pytest.fixture(scope="function")
+def driver():
+    options = UiAutomator2Options()
+    options.load_capabilities({
+        "platformName": "Android",
+        "platformVersion": "13.0",
+        "deviceName": "Samsung Galaxy S23",
         "app": "bs://sample.app",
-
-        # Set other BrowserStack capabilities
-        'bstack:options': {
+        "bstack:options": {
+            "userName": BROWSERSTACK_USERNAME,
+            "accessKey": BROWSERSTACK_ACCESS_KEY,
             "projectName": "First Python project",
-            "buildName": "browserstack-build-1",
-            "sessionName": "BStack first_test",
-
-            # Set your access credentials
-            "userName": "iakivkramarenko_qKHOLN",
-            "accessKey": "FSHAmndKHW3XsDkgm5zT"
+            "buildName": "browserstack-build-2",
+            "sessionName": "BStack home_work"
         }
     })
 
-    # browser.config.driver = webdriver.Remote("http://hub.browserstack.com/wd/hub", options=options)
-    browser.config.driver_remote_url = 'http://hub.browserstack.com/wd/hub'
-    browser.config.driver_options = options
+    driver = webdriver.Remote("http://hub.browserstack.com/wd/hub", options=options)
+    driver.implicitly_wait(10)
 
-    browser.config.timeout = float(os.getenv('timeout', '10.0'))
+    yield driver
 
-    yield
+    try:
+        screenshot = driver.get_screenshot_as_png()
+        allure.attach(screenshot, name="screenshot_final", attachment_type=allure.attachment_type.PNG)
 
-    browser.quit()
+        page_source = driver.page_source
+        allure.attach(page_source, name="page_source_final", attachment_type=allure.attachment_type.XML)
+
+        logs = driver.get_log('logcat')
+        allure.attach(str(logs), name="logcat_final", attachment_type=allure.attachment_type.TEXT)
+
+        session_id = driver.session_id
+        video_url = get_browserstack_video_url(session_id)
+        if video_url:
+            video_response = requests.get(video_url)
+            if video_response.status_code == 200:
+                allure.attach(video_response.content, name="test_execution_video", attachment_type=allure.attachment_type.MP4)
+
+    except Exception:
+        pass
+
+    driver.quit()
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    if rep.when == "call" and rep.failed:
+        driver = item.funcargs.get("driver")
+        if driver:
+            try:
+                screenshot = driver.get_screenshot_as_png()
+                allure.attach(screenshot, name="screenshot_on_failure", attachment_type=allure.attachment_type.PNG)
+            except Exception:
+                pass
